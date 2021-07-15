9db83cffbac476b5464467defe9cbd0fc2109e8c
rong.wang <345001183@qq.com>
2021-07-15 19:50:09 +0800

    element ui项目初始化

---
+++ element/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <a href="/demo">点我显示</a>
+    <router-view/>
+<!--    <demo/>-->
+  </div>
+</template>
+
+<script>
+import Demo from "./components/Demo";
+
+export default {
+  name: 'App',
+  components:{
+    Demo
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ element/src/components/Demo.vue
@@ -0,0 +1,13 @@
+<template>
+  <h3>this is a demo page</h3>
+</template>
+
+<script>
+export default {
+  name: "Demo"
+}
+</script>
+
+<style scoped>
+
+</style>
